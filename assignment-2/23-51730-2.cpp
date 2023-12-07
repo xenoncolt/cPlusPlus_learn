f16495e41bb99cddc4130520e5eeea229765a933
Md. Shahriar Haque <81591350+xenoncolt@users.noreply.github.com>
2023-12-07 20:18:45 +0600

    Add book details input and display functionality

---
+++ assignment-2/23-51730-2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::string;
+
+class inputBookDetails {
+        public:
+        string title;
+        string author;
+        int year;
+
+        void inputTitle() {
+            std::getline(cin, title);
+        }
+
+        void inputAuthor() {
+            std::getline(cin, author);
+        }
+
+        void inputYear() {
+            cin >> year;
+        }
+    
+};
+
+
+class displayBookDetails : public inputBookDetails {
+    public:
+        void askTitle() {
+            cout << "Which book you are looking for?\n";
+        }
+
+        void askAuthor() {
+            cout << "There are many book with that name.\nCan you please type the author name of that book?\n";
+        }
+
+        void askYear() {
+            cout << "Sorry, still there are few book with that title and author name.\nCan you please type the publication year?\n";
+        }
+
+        void display() {
+            cout << "The book you are looking for is: \n" << title << " (" << year << ")" << "\n            written by " << author << "\n";
+        }
+
+};
+
+int main() {
+    displayBookDetails bookDetails;
+
+    bookDetails.askTitle();
+    bookDetails.inputTitle();
+
+    bookDetails.askAuthor();
+    bookDetails.inputAuthor();
+
+    bookDetails.askYear();
+    bookDetails.inputYear();
+
+    bookDetails.display();
+};
